78ec066294fa4b3eae433b3ab7b7f91199d3b675
ambrecht <tino@ambrecht.de>
2025-03-25 17:52:26 +0100

    hidden input android

--- app/type/typewriter.tsx
+++ app/type/typewriter.tsx
@@ -208,7 +208,7 @@ export default function Typewriter() {
       <input
         ref={hiddenInputRef}
         type="text"
-        className="opacity-1 absolute top-0 left-0 w-1 h-1 pointer-events-none"
+        className="absolute -left-[10000px] "
         onKeyDown={handleKeyDown}
         autoFocus
         tabIndex={-1}
